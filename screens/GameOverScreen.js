ef943dab61580e015c68a8eb96249e932ddd4ccc
Sameera Abeysekara <you@example.comabeysekaramadushanka14@gmail.com>
2020-03-22 14:24:55 +0530

    Adding Custom Fonts

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import {View, Text,StyleSheet} from 'react-native';
+import {View, Text,StyleSheet,Button} from 'react-native';
 const GameOverScreen = props =>{
     return (<View style={styles.screen}>
         <Text>The Game is Over</Text>
+    <Text>Number of rounds : {props.roundsNumber}</Text>
+    <Text>Number was : {props.userNumber}</Text>
+    <Button title="New Game" onPress={props.onRestart} />
     </View>);
 };
 
--- screens/StartGamesScreen.js
+++ screens/StartGamesScreen.js
@@ -74,7 +74,8 @@ let confirmedOutput;
 const styles = StyleSheet.create({
     title:{
         fontSize:20,
-        marginVertical:10
+        marginVertical:10,
+        fontFamily:'open-sans-bold'
     },
     inputContainer:{
         width:300,

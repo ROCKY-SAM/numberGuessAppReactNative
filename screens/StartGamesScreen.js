2881c4942d0fb1872382b313912d0a7cba7cffa9
Sameera Abeysekara <you@example.comabeysekaramadushanka14@gmail.com>
2020-04-05 00:54:22 +0530

    Controlling Orientation & Using the

--- screens/StartGamesScreen.js
+++ screens/StartGamesScreen.js
@@ -7,7 +7,8 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
   Alert,
-  Dimensions
+  Dimensions,ScrollView,
+  KeyboardAvoidingView
 } from 'react-native';
 
 import Card from '../components/Card';
@@ -61,6 +62,8 @@ const StartGameScreen = props => {
   }
 
   return (
+    <ScrollView>
+      <KeyboardAvoidingView behavior="position" keyboardVerticalOffset={30}>
     <TouchableWithoutFeedback
       onPress={() => {
         Keyboard.dismiss();
@@ -100,6 +103,8 @@ const StartGameScreen = props => {
         {confirmedOutput}
       </View>
     </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+    </ScrollView>
   );
 };
 

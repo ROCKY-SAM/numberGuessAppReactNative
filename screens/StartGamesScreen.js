06bc3e9009a74c6975787fa9b413b1234c898946
Sameera Abeysekara <you@example.comabeysekaramadushanka14@gmail.com>
2020-04-05 01:05:00 +0530

    Listening to Orientation Changes

--- screens/StartGamesScreen.js
+++ screens/StartGamesScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import {
   View,
   Text,
@@ -22,6 +22,18 @@ const StartGameScreen = props => {
   const [enteredValue, setEnteredValue] = useState('');
   const [confirmed, setConfirmed] = useState(false);
   const [selectedNumber, setSelectedNumber] = useState();
+
+  const [buttonWidth,setButtonWidth]=useState(Dimensions.get('window').width / 4);
+
+useEffect(()=>{
+  const updateLayout =() =>{
+    setButtonWidth(Dimensions.get('window').width / 4);
+  };
+  Dimensions.addEventListener('change',updateLayout);
+  return () =>{
+    Dimensions.removeEventListener('change',updateLayout);
+  };
+});
 
   const numberInputHandler = inputText => {
     setEnteredValue(inputText.replace(/[^0-9]/g, ''));
@@ -84,14 +96,14 @@ const StartGameScreen = props => {
             value={enteredValue}
           />
           <View style={styles.buttonContainer}>
-            <View style={styles.button}>
+            <View style={{width:buttonWidth}}>
               <Button
                 title="Reset"
                 onPress={resetInputHandler}
                 color={Colors.accent}
               />
             </View>
-            <View style={styles.button}>
+            <View style={{width:buttonWidth}}>
               <Button
                 title="Confirm"
                 onPress={confirmInputHandler}

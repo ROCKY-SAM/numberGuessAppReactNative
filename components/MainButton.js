7c83b98d6f4a4e31a0b348cc47d3e948987d398f
Sameera Abeysekara <you@example.comabeysekaramadushanka14@gmail.com>
2020-04-06 07:28:46 +0530

    Working with Platform.select()

--- components/MainButton.js
+++ components/MainButton.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import {View,Text,StyleSheet ,TouchableOpacity} from 'react-native';
-import Colors from '../constants/colors'
+import {View,Text,StyleSheet ,TouchableOpacity,TouchableWithoutFeedback, Platform} from 'react-native';
+import Colors from '../constants/colors';
+
+
+
+
 const MainButton = props =>{
-return <TouchableOpacity activeOpacity={0.6} onPress={props.onPress}>
+
+let ButtonComponent = TouchableOpacity;
+
+if(Platform.OS === 'android' && Platform.Version >= 21){
+    ButtonComponent = TouchableWithoutFeedback;
+}
+
+
+
+return <View style={styles.buttonContainer}> <ButtonComponent activeOpacity={0.6} onPress={props.onPress}>
     <View style={styles.button}>
 <Text style={styles.butotnText}>{props.children}</Text>
     </View>
-</TouchableOpacity>
+</ButtonComponent></View>
 };
 
 const styles = StyleSheet.create({
+    buttonContainer:{
+    borderRadius:25,
+    overflow:'hidden'
+},
     button:{
         backgroundColor : Colors.primary,
         paddingVertical:12,
--- screens/StartGamesScreen.js
+++ screens/StartGamesScreen.js
@@ -73,49 +73,49 @@ useEffect(()=>{
     );
   }
 
-  return (
+   return (
     <ScrollView>
       <KeyboardAvoidingView behavior="position" keyboardVerticalOffset={30}>
-    <TouchableWithoutFeedback
-      onPress={() => {
-        Keyboard.dismiss();
-      }}
-    >
-      <View style={styles.screen}>
-        <TitleText style={styles.title}>Start a New Game!</TitleText>
-        <Card style={styles.inputContainer}>
-          <BodyText>Select a Number</BodyText>
-          <Input
-            style={styles.input}
-            blurOnSubmit
-            autoCapitalize="none"
-            autoCorrect={false}
-            keyboardType="number-pad"
-            maxLength={2}
-            onChangeText={numberInputHandler}
-            value={enteredValue}
-          />
-          <View style={styles.buttonContainer}>
-            <View style={{width:buttonWidth}}>
-              <Button
-                title="Reset"
-                onPress={resetInputHandler}
-                color={Colors.accent}
+        <TouchableWithoutFeedback
+          onPress={() => {
+            Keyboard.dismiss();
+          }}
+        >
+          <View style={styles.screen}>
+            <TitleText style={styles.title}>Start a New Game!</TitleText>
+            <Card style={styles.inputContainer}>
+              <BodyText>Select a Number</BodyText>
+              <Input
+                style={styles.input}
+                blurOnSubmit
+                autoCapitalize="none"
+                autoCorrect={false}
+                keyboardType="number-pad"
+                maxLength={2}
+                onChangeText={numberInputHandler}
+                value={enteredValue}
               />
-            </View>
-            <View style={{width:buttonWidth}}>
-              <Button
-                title="Confirm"
-                onPress={confirmInputHandler}
-                color={Colors.primary}
-              />
-            </View>
+              <View style={styles.buttonContainer}>
+                <View style={{width: buttonWidth}}>
+                  <Button
+                    title="Reset"
+                    onPress={resetInputHandler}
+                    color={Colors.accent}
+                  />
+                </View>
+                <View style={{width: buttonWidth}}>
+                  <Button
+                    title="Confirm"
+                    onPress={confirmInputHandler}
+                    color={Colors.primary}
+                  />
+                </View>
+              </View>
+            </Card>
+            {confirmedOutput}
           </View>
-        </Card>
-        {confirmedOutput}
-      </View>
-    </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
     </ScrollView>
   );
 };
